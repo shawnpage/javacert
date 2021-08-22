59df9ec341da9cf5d36218ffafdecb2d40b3f66b
Shawn Page <shawn.page@gmail.com>
2021-08-21 23:27:35 -0400

    Added chapter 1 practice files.

---
+++ chapter1/BadFloat.java
@@ -0,0 +1,7 @@
+public class BadFloat {
+    public static void main(String[] args) {
+
+        // does not compile
+        float value = 102.0;
+    }
+}
---
+++ chapter1/TestClass.java
@@ -0,0 +1,9 @@
+// Add any necessary import statements here
+public class TestClass {
+   public static void main(String[] args) {
+      // Add test code here
+ 
+      // Add any print statements here
+      System.out.println("Hello World!");
+   }
+}
