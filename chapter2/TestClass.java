a20a0d5b8d437db7c3a4823dc5d55ace4ddde060
Shawn Page <shawn.page@gmail.com>
2021-09-18 13:12:30 -0400

    Minor additions

--- chapter2/TestClass.java
+++ chapter2/TestClass.java
@@ -3,13 +3,34 @@
 
 
 public class TestClass {
-   public static void main(String[] args) {
+
+    public static void doesThisCompile(boolean check) {
+        var question = 0;
+        question = 1;
+        var answer = -1;
+        if (check) {
+            answer = 2;
+        } else {
+            answer = 3;
+        }
+        System.out.println(answer);
+    }
+
+    // public void twoTypes() {
+    //     int a, var b = 3;  // DOES NOT COMPILE
+    //     var n = null;      // DOES NOT COMPILE
+    // }
+    
+    public static void main(String[] args) {
       // Add test code here
  
       // Add any print statements here
 
-
        int mythis = 10;
-      System.out.println("Hello World!");
+       System.out.println("Hello World!");
+
+       boolean check = false;
+       doesThisCompile(check);
+       
    }
 }
